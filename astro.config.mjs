f8a98eabbbd3cf9f6685350ce68e8cc275068096
hadagaga <3258009025@qq.com>
2026-06-28 21:18:04 +0800

    fix custom domain metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hada.asia',
+	site: 'https://computer.hada.asia',
 	integrations: [
 		starlight({
 			title: '计算机体系视频百科',
